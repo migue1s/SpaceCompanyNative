45a8df876981de09d07832b199949eb3b0f67a63
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-15 19:28:33 -0400

    Adds the resource storybooks

--- src/components/Resource.stories.tsx
+++ src/components/Resource.stories.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ResourceBullet from './ResourceBullet';
 import {storiesOf} from '@storybook/react-native';
+import ResourceMachine from './ResourceMachine';
+import ResourceSummary from './ResourceSummary';
+import ResourceTile from './ResourceTile';
 
-storiesOf('Resources', module).add('to Storybook', () => <ResourceBullet />);
+storiesOf('Resources', module).add('Bullet', () => <ResourceBullet />);
+storiesOf('Resources', module).add('Machine', () => <ResourceMachine />);
+storiesOf('Resources', module).add('Summary', () => <ResourceSummary />);
+storiesOf('Resources', module).add('Tile', () => <ResourceTile />);
--- storybook/index.tsx
+++ storybook/index.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
+import {View} from 'react-native';
 import {AppRegistry} from 'react-native';
 import {getStorybookUI, configure} from '@storybook/react-native';
+import {addDecorator} from '@storybook/react';
 
 import './rn-addons';
 
@@ -7,6 +10,16 @@ import './rn-addons';
 configure(() => {
   require('../src/components/Resource.stories');
 }, module);
+
+addDecorator((fn) => (
+  <View
+    style={{
+      padding: 20,
+      width: '100%',
+    }}>
+    {fn()}
+  </View>
+));
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
