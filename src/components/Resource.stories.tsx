8f2f0788bb242bf5a59d7af70bc5bad45bf95050
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-15 16:57:56 -0400

    Adds skeleton for resource story

---
+++ src/components/Resource.stories.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ResourceBullet from './ResourceBullet';
+import { storiesOf } from '@storybook/react-native';
+
+storiesOf('Welcome', module).add('to Storybook', () => <ResourceBullet/>);
--- storybook/addons.js
+++ storybook/addons.js
@@ -1,3 +1,2 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
-import '@storybook/preset-create-react-app';
