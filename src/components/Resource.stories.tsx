8f578d5bbe62bfce18731a02288ef106c38f1273
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-22 07:51:00 -0400

    Implements the ResourceBullet

--- src/components/Resource.stories.tsx
+++ src/components/Resource.stories.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import {View} from 'react-native';
 import ResourceBullet from './ResourceBullet';
 import {storiesOf} from '@storybook/react-native';
 import ResourceMachine from './ResourceMachine';
 import ResourceSummary from './ResourceSummary';
 import ResourceTile from './ResourceTile';
 
-storiesOf('Resources', module).add('Bullet', () => <ResourceBullet />);
+storiesOf('Resources', module).add('Bullet', () => (
+  <View>
+    <ResourceBullet cost={10} current={1} dps={3} name="Metal" storage={50} />
+    <ResourceBullet cost={10} current={1} dps={0} name="Metal" storage={20} />
+    <ResourceBullet cost={10} current={1} dps={0} name="Metal" storage={5} />
+    <ResourceBullet cost={10} current={10} dps={0} name="Metal" storage={10} />
+  </View>
+));
 storiesOf('Resources', module).add('Machine', () => <ResourceMachine />);
 storiesOf('Resources', module).add('Summary', () => <ResourceSummary />);
 storiesOf('Resources', module).add('Tile', () => <ResourceTile />);
--- src/components/ResourceBullet.tsx
+++ src/components/ResourceBullet.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-const ResourceCost = () => {
-  return <Text>Metal: 2 (Done!)</Text>;
+interface ResourceCostProps {
+  name: string;
+  dps: number;
+  cost: number;
+  storage: number;
+  current: number;
+}
+
+const ResourceCost = ({
+  name,
+  dps,
+  cost,
+  current,
+  storage,
+}: ResourceCostProps) => {
+  let info = '';
+  if (current >= cost) {
+    info = 'done!';
+  } else if (cost > storage) {
+    info = 'insufficient storage';
+  } else if (dps <= 0) {
+    info = '~~';
+  } else {
+    const remaining = cost - current;
+    const seconds = remaining / dps;
+    info = '' + seconds;
+  }
+  return (
+    <Text>
+      {name}: {current} ({info})
+    </Text>
+  );
 };
 
 export default ResourceCost;
