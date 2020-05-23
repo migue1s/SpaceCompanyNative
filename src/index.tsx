7a69bbd270252ac91fb07cfac3b4a3dfaeae3750
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-22 20:11:26 -0400

    Adds storybooks as a button

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,34 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Resources from './containers/Resources';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import storybook from '../storybook';
+import {Text} from 'react-native';
 
-const Stack = createStackNavigator();
+type RootStackParamList = {
+  Storybooks: undefined;
+  Resources: undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Resources">
-        <Stack.Screen name="Resources" component={Resources} />
-      </Stack.Navigator>
+      <RootStack.Navigator initialRouteName="Resources">
+        <RootStack.Screen name="Storybooks" component={storybook} />
+        <RootStack.Screen
+          name="Resources"
+          component={Resources}
+          options={({navigation}) => ({
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate('Storybooks')}>
+                <Text>Components</Text>
+              </TouchableOpacity>
+            ),
+          })}
+        />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
