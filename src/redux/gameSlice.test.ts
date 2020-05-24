ca55cc0b19cebebddf99dc8b462386af35aecfd5
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-24 08:39:48 -0400

    Makes tests stateful

--- src/redux/gameSlice.test.ts
+++ src/redux/gameSlice.test.ts
@@ -1,16 +1,21 @@
 import game, {initialState, manualGain} from './gameSlice';
 
 describe('game reducer', () => {
+  let gameState = game(initialState, {type: 'test'});
+
+  beforeEach(() => {
+    gameState = game(initialState, {type: 'test'});
+  });
+
   it('should handle the initial state', () => {
-    expect(game(initialState, {type: 'test'})).toEqual(initialState);
+    expect(initialState).toEqual(initialState);
   });
 
   it('should increase resources by one', () => {
-    expect(game(initialState, {type: 'test'}).resources.metal.current).toEqual(
-      0,
-    );
-    expect(
-      game(initialState, manualGain('metal')).resources.metal.current,
-    ).toEqual(1);
+    expect(gameState.resources.metal.current).toEqual(0);
+    gameState = game(gameState, manualGain('metal'));
+    expect(gameState.resources.metal.current).toEqual(1);
+    gameState = game(gameState, manualGain('metal'));
+    expect(gameState.resources.metal.current).toEqual(2);
   });
 });
