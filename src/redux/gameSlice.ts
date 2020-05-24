a7fd9d50709edb8e9108dcffd37ddeda037c7ce3
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-24 07:59:16 -0400

    Extracts magic numbers from calculations

--- src/redux/gameSlice.ts
+++ src/redux/gameSlice.ts
@@ -2,6 +2,14 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {resourcesData} from '../data/resourcesData';
 import {ResourceType} from '../types';
 
+const gain = 1;
+
+// 0 = no storage carried over, 1 = all storage kept
+const storageEfficiencyMultiplier = 0;
+
+// 2 = multiply stoage by 2 when growing
+const storageGrowthMultiplier = 2;
+
 const gameSlice = createSlice({
   name: 'game',
   initialState: {
@@ -19,13 +27,19 @@ const gameSlice = createSlice({
       });
       return state;
     },
+    manualGain: (state, action: PayloadAction<ResourceType>) => {
+      state.resources[action.payload].current = Math.min(
+        state.resources[action.payload].capacity,
+        state.resources[action.payload].current + gain,
+      );
+    },
     upgradeStorage: (state, action: PayloadAction<ResourceType>) => {
       if (
         state.resources[action.payload].current ===
         state.resources[action.payload].capacity
       ) {
-        state.resources[action.payload].current = 0;
-        state.resources[action.payload].capacity *= 2;
+        state.resources[action.payload].current *= storageEfficiencyMultiplier;
+        state.resources[action.payload].capacity *= storageGrowthMultiplier;
       }
     },
   },
