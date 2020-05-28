d67f3a855fc072bc38f09661695cad09f9cce418
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-28 07:24:27 -0400

    Fixes linter

--- src/components/ResourceBullet.tsx
+++ src/components/ResourceBullet.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {Text} from 'react-native';
 import {durationFormatter} from '../utils/TimeFormatter';
 
 interface ResourceCostProps {
--- src/components/ResourceRow.tsx
+++ src/components/ResourceRow.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import {ResourceType} from '../types';
 import {useResourceCount, useResource} from '../hooks';
 
