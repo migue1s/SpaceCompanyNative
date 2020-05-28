b255d4c13c0212cb7be9f32220b1b0a75db4831b
Leonard Mandtler <lmandtler@outlook.de>
2020-05-28 19:48:41 +0200

    Style ResourceMachine

--- src/components/ResourceMachine.tsx
+++ src/components/ResourceMachine.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
+import {Machine, ResourceType} from '../types';
+import GlobalStyles from '../styles/GlobalStyles';
+import ResourceStyles from '../styles/ResourceStyles';
 
-const ResourceMachine = () => {
+const getCosts = (costs: {[x in ResourceType]?: number}) => {
   return (
-    <View>
-      <Text>Miner: 2</Text>
-      <Text>Build a pickaxe for your miner.</Text>
-      <Text>Cost:</Text>
-      <Text>Input:</Text>
-      <Text>Output:</Text>
-      <TouchableOpacity>
-        <Text>Get 1</Text>
-      </TouchableOpacity>
+    <View style={ResourceStyles.machineBox}>
+      <Text style={GlobalStyles.property}>Cost:</Text>
+    </View>
+  );
+};
+
+const ResourceMachine = ({machine}: {machine: Machine}) => {
+  return (
+    <View style={ResourceStyles.machineCard}>
+      <View>
+        <Text style={GlobalStyles.header}>{machine.name}</Text>
+        <Text>{machine.desc}</Text>
+      </View>
+      <View style={ResourceStyles.machineContainer}>
+        {getCosts(machine.cost)}
+        <View style={ResourceStyles.machineBox}>
+          <Text style={GlobalStyles.property}>Input:</Text>
+          <Text style={GlobalStyles.property}>Output:</Text>
+        </View>
+      </View>
+      <View>
+        <TouchableOpacity style={GlobalStyles.button}>
+          <Text>Get 1</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
---
+++ src/styles/ResourceStyles.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const ResourceStyles = StyleSheet.create({
+  machineCard: {
+    alignSelf: 'center',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+  },
+  machineContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
+  },
+  machineBox: {
+    width: '50%',
+  },
+});
+
+export default ResourceStyles;
