dcb4e8c62b6ba03dc04fdaa3a4860b3d2f15098b
Leonard Mandtler <lmandtler@outlook.de>
2020-05-31 12:29:04 +0200

    Connect ResourceMachine to stories

--- src/components/ResourceMachine.tsx
+++ src/components/ResourceMachine.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
-import {Machine, ResourceType} from '../types';
+import {Machine} from '../types';
 import GlobalStyles from '../styles/GlobalStyles';
 import ResourceStyles from '../styles/ResourceStyles';
-
-const getCosts = (costs: {[x in ResourceType]?: number}) => {
-  return (
-    <View style={ResourceStyles.machineBox}>
-      <Text style={GlobalStyles.property}>Cost:</Text>
-    </View>
-  );
-};
+import ResourceCost from './ResourceBullet';
+import {useResource} from '../hooks';
 
 const ResourceMachine = ({machine}: {machine: Machine}) => {
   return (
@@ -20,7 +14,9 @@ const ResourceMachine = ({machine}: {machine: Machine}) => {
         <Text>{machine.desc}</Text>
       </View>
       <View style={ResourceStyles.machineContainer}>
-        {getCosts(machine.cost)}
+        {Object.keys(machine.cost).map(resource => (
+          <ResourceCost resource. dps cost current storage />
+        ))}
         <View style={ResourceStyles.machineBox}>
           <Text style={GlobalStyles.property}>Input:</Text>
           <Text style={GlobalStyles.property}>Output:</Text>
