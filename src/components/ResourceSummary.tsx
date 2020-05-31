92c3059f42d0f73092de0d6557347bdbf08081b3
Miguel Saiz <miguelsaizo@gmail.com>
2020-05-31 10:10:14 -0400

    Adds the resource summary

--- src/components/ResourceSummary.tsx
+++ src/components/ResourceSummary.tsx
@@ -1,24 +1,34 @@
 import React, {useCallback} from 'react';
 import {TouchableOpacity} from 'react-native';
 import {ResourceType} from '../types';
-import {useResource} from '../hooks';
+import {useResource, useResourceCount} from '../hooks';
 import ThemedView from './ThemedView';
 import ThemedText from './ThemedText';
 import {useDispatch} from 'react-redux';
 import {manualGain} from '../redux/gameSlice';
+import {durationFormatter} from '../utils/TimeFormatter';
 
 const ResourceSummary = ({type}: {type: ResourceType}) => {
   const dispatch = useDispatch();
   const resource = useResource(type);
+  const resourceCount = useResourceCount(type);
   const onGainPress = useCallback(() => {
     dispatch(manualGain(type));
   }, [dispatch, type]);
+
+  const secondsUntilFull =
+    resourceCount.perSecondDisplay !== 0
+      ? durationFormatter(
+          (resourceCount.capacity - resourceCount.current) /
+            resourceCount.perSecondDisplay,
+        )
+      : 'N/A';
 
   return (
     <ThemedView>
       <ThemedText variant="body">{resource.desc}</ThemedText>
       <ThemedText variant="body" style={{paddingVertical: 20}}>
-        Time remaining until full storage:{' '}
+        {`Time remaining until full storage: ${secondsUntilFull}`}
       </ThemedText>
       <TouchableOpacity onPress={onGainPress}>
         <ThemedText variant="body">Gain 1</ThemedText>
