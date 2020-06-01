c895e3d8e3cd55d89abba837226f3416353fd0ad
Leonard Mandtler <lmandtler@outlook.de>
2020-06-01 05:35:02 +0200

    Implemented first mock of progress bar.

---
+++ src/components/ThemedProgressBar.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {StyleSheet, View, ViewProps, ViewStyle, Text} from 'react-native';
+import {useTheme} from '../hooks';
+import {ThemeVariant} from '../types';
+
+const styles = StyleSheet.create({
+  progressBarContainer: {
+    alignSelf: 'center',
+    flexDirection: 'row',
+    height: 20,
+    width: '100%',
+    borderWidth: 1,
+  },
+  progressBarFiller: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+});
+
+const themeStylesContainer = StyleSheet.create<
+  {[x in ThemeVariant]: ViewStyle}
+>({
+  dark: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+  },
+  light: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+  },
+});
+
+const themeStylesFiller = StyleSheet.create<{[x in ThemeVariant]: ViewStyle}>({
+  dark: {
+    backgroundColor: 'skyblue',
+  },
+  light: {
+    backgroundColor: 'skyblue',
+  },
+});
+
+const ThemedProgressBar = ({
+  style = {},
+  current,
+  total,
+  ...rest
+}: ViewProps & {current: number; total: number}) => {
+  const theme = useTheme();
+  const progress = (current: number, total: number): string => {
+    return ((current / total) * 100).toString() + '%';
+  };
+
+  return (
+    <View
+      style={[style, styles.progressBarContainer, themeStylesContainer[theme]]}
+      {...rest}>
+      <View
+        style={[
+          StyleSheet.absoluteFill,
+          style,
+          styles.progressBarFiller,
+          themeStylesFiller[theme],
+          {width: progress(current, total)},
+        ]}
+      />
+    </View>
+  );
+};
+
+export default ThemedProgressBar;
