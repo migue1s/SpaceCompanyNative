c7196d53764f9b992b544c96758e45721a1f1c63
Leonard Mandtler <lmandtler@outlook.de>
2020-06-01 05:56:02 +0200

    clean up progressBar

--- src/components/ThemedProgressBar.tsx
+++ src/components/ThemedProgressBar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {StyleSheet, View, ViewProps, ViewStyle, Text} from 'react-native';
 import {useTheme} from '../hooks';
 import {ThemeVariant} from '../types';
-import {round} from 'react-native-reanimated';
 
 const styles = StyleSheet.create({
   progressBarContainer: {
